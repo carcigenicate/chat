e282b00abfc708954cdf1247ef689dcf4f3347db
carcigenicate <slomifier@gmail.com>
2017-09-26 18:50:41 -0600

    Continued on with the graphics client.

--- src/chat/client.clj
+++ src/chat/client.clj
@@ -5,7 +5,7 @@
             [chat.testing.simple-server :as ss]
             [chat.testing.helpers :as ch]
 
-            [clojure.core.async :refer [thread chan]]
+            [clojure.core.async :refer [thread chan go >!! <! alts! timeout]]
             [chat.message :as m])
 
   (:import [java.net Socket SocketException]))
@@ -13,46 +13,46 @@
 (def test-address "localhost")
 (def test-port ss/test-port)
 
-; TODO: Add a prompt to ask for a message to send
+; FIXME: Obviously a terrible idea
+(def client-id (atom (str (rand-int 1000))))
 
-(defrecord User [server-sock username running?!])
+(def message-buffer-size 100)
+(def incoming-message-check-delay 500)
+(def outgoing-message-check-delay incoming-message-check-delay)
+
+(defrecord User [server-sock username])
+
+(def running?! (atom true))
+; TODO: Change to refs of vectors?
+(def incoming-message-chan (chan message-buffer-size))
+(def outgoing-message-chan (chan message-buffer-size))
 
 (defn new-user [server-sock username]
-  (->User server-sock username (atom true)))
+  (->User server-sock username))
 
-(defn ask-for-message []
-  (ch/print-fl ">: ")
-  (read-line))
+; TODO: TEST! Make a simple button that sends, and print incoming.
+; TODO: Make a third mode: Graphics client
+(defn connection-handler [server-sock]
+  (let [user (new-user (bs/new-buffered-socket server-sock) @client-id)
+        {:keys [server-sock username]} user]
 
-(defn parse-format-message [raw-message]
-  (let [{:keys [sender text]} (m/parse-message raw-message)]
-    (str sender ": " text)))
-
-(defn connection-handler [server]
-  (ch/print-fl "Name?: ")
-  (let [b-sock (bs/new-buffered-socket server)
-        entered-name (read-line)
-        user (new-user b-sock entered-name)
-        {:keys [server-sock running?! username]} user]
-
-    (bs/write server-sock username)
-
-    (try
+    ; Outgoing Message Listener
+    (go
       (while @running?!
-        (let [sending-msg (ask-for-message)]
+        (let [t-o (timeout outgoing-message-check-delay)
+              [result port] (alts! [outgoing-message-chan t-o])]
 
-          (when (> (count sending-msg) 1)
-             (bs/write server-sock sending-msg))
+          (when-not (= port t-o)
+            (bs/write server-sock result)))))
 
-          (when-let [recieved (bs/read-lines server-sock)]
-            (doseq [msg recieved]
-              (println "\t" (parse-format-message msg))))))
+    ; Incoming Message Listener
+    (thread
+      (while @running?!
+        (when (bs/data-ready? server-sock)
+          (>!! incoming-message-chan
+               (bs/read-line server-sock)))
 
-      (catch SocketException se
-        (println "Exception:" (.getMessage se) "\nClosing..."))
-
-      (finally
-        (bs/close server-sock)))))
+        (Thread/sleep incoming-message-check-delay)))))
 
 (defn connect [address port]
   (nh/connect-to address port connection-handler))
